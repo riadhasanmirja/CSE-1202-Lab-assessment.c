1bdc9cd7bad4a11aa2cd88ad8e87a8801c119d46
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2026-03-04 21:24:55 +0600

    Create loop ***** ***** ***** ***** *****.c

---
+++ loop ***** ***** ***** ***** *****.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+          int a,i;
+
+          for(i=1;i<=5;i++)
+                    {
+                    for(a=1;a<=5;a++)
+                              {
+                              printf(" *");
+                    }
+                    printf("\n");
+          }
+          return 0;
+}
