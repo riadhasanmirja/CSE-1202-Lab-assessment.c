d06df827e142e9907744646585dd019b2dcaa727
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2026-03-04 21:11:32 +0600

    Create loop 12345 1234 123 12 1.c

---
+++ loop 12345 1234 123 12 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+          int a,i;
+
+          for(i=5;i>=1;i--)
+                    {
+                    for(a=1;a<=i;a++)
+                              {
+                              printf("%d",a);
+                    }
+                    printf("\n");
+          }
+          return 0;
+}
