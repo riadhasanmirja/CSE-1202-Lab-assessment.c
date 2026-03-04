e1bf011966d8de95bed8da082eab15c9f8befb86
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2026-03-04 21:15:57 +0600

    Add program to print numbers in increasing pattern

---
+++ loop 1 22 333 4444 55555.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+          int a,i;
+
+          for(i=1;i<=5;i++)
+                    {
+                    for(a=1;a<=i;a++)
+                              {
+                              printf("%d",i);
+                    }
+                    printf("\n");
+          }
+          return 0;
+}
